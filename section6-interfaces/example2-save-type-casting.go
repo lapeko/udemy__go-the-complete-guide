78e34cef40d33e773683ef009126c28ebd4ba651
lapeko <lapeko88@gmail.com>
2025-01-14 16:35:41 +0100

    play with refrelction and types

--- section6-interfaces/example2-save-type-casting.go
+++ section6-interfaces/example2-save-type-casting.go
@@ -1,8 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func addTwoAndPrintIfNum(data interface{}) {
+	fmt.Println("func addTwoAndPrintIfNum")
 	if num, ok := data.(int); ok {
 		fmt.Println(num + 2)
 	} else {
@@ -10,7 +14,21 @@ func addTwoAndPrintIfNum(data interface{}) {
 	}
 }
 
+func printType(data interface{}) {
+	fmt.Printf("func printType\nIs int: %t\n", reflect.TypeOf(data).Kind() == reflect.Int)
+	fmt.Print("The type is ")
+	switch data.(type) {
+	case int:
+		fmt.Println("int")
+	case string:
+		fmt.Println("string")
+	default:
+		fmt.Println("neither int nor string")
+	}
+}
+
 func main() {
 	addTwoAndPrintIfNum(struct{ test int }{test: 123})
 	addTwoAndPrintIfNum(10)
+	printType(10)
 }
