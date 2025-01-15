bc76065b2b0c059d256cc6b3107acca0643e7405
lapeko <lapeko88@gmail.com>
2025-01-15 18:07:33 +0100

    use functions as first class objects

---
+++ section8-funcs-deep-dive/exercise1.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+type number interface {
+	int | int8 | int16 | int32 | int64 | float32 | float64
+}
+
+func multiplyBy[T number](num T, multiplier T) T {
+	return num * multiplier
+}
+
+func filterBy[T number](arr *[]T, filter func(T) bool) []T {
+	newArr := (*arr)[0:0]
+	for _, val := range *arr {
+		if filter(val) {
+			newArr = append(newArr, val)
+		}
+	}
+	return newArr
+}
+
+func main() {
+	nums := []int{1, 2, 3, 4, 5, 6, 7}
+	odds := filterBy(&nums, func(num int) bool {
+		return num%2 != 0
+	})
+	for _, value := range odds {
+		fmt.Printf("doubled odd num of %d is %d\n", value, multiplyBy(value, 2))
+	}
+}
